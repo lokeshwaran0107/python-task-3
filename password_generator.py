61b85929f34d478b25b845c5f0d83ddccefb13ee
Lokeshwaran <lokeshwaran0107@gmail.com>
2024-08-23 22:25:59 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,18 @@
+import random
+import string
+def password_generator(length):
+    all_characters = string.ascii_letters + string.digits + string.punctuation
+    if length < 8:
+        print("Password length should be at least 8 characters.")
+        return None
+    password = ''.join(random.choice(all_characters) for i in range(length))
+    return password
+
+def main():
+    length = int(input("Enter the desired length of the password: "))
+    password = password_generator(length)
+    if password:
+        print("Generated Password : ", password)
+
+if __name__ == "__main__":
+    main()
